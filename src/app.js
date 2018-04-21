3f7879523a6b951760013709c2b38e053bc0bbe2
Chris Amora <christiano.amora@gmail.com>
2018-04-21 14:53:09 -0300

    simple webpack conf with babel

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Jsx = () => {
+  return (
+    <div>It's on Babbeee</div>
+  )
+}
+
+
+ReactDOM.render(<Jsx />, document.querySelector('#app'))
---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require('path');
+console.log('oi');
+module.exports = {
+  mode: 'development',
+  entry: './src/app.js',
+  output: {
+    path: path.join(__dirname, 'public'),
+    filename: 'bundle.js'
+  },
+  module: {
+    rules: [
+      {
+        loader: 'babel-loader',
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
+  },
+
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: path.join(__dirname, 'public'),
+    historyApiFallback: true
+  }
+};
