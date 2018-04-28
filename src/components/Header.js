d5d636a6137401ee95f7f2dc0eb9b8d6aadd4c93
Chris Amora <christiano.amora@gmail.com>
2018-04-28 19:58:21 -0300

    added particles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,85 @@
 import React from 'react';
 
-export default () => (
-    <div>I am a Header!!!!@@@</div>
-);
+import Particles from 'react-particles-js';
+
+export default class App extends React.Component {
+  render() {
+    console.log(this);
+    return (
+      <Particles params={
+        {
+          particles: {
+            number: {
+              value: 90,
+              density: {
+                enable: true,
+                value_area: 800,
+              },
+            },
+            color: {
+              value: '#ffffff',
+            },
+            shape: {
+              type: 'circle',
+              stroke: {
+                width: 0,
+                color: '#000000',
+              },
+              polygon: {
+                nb_sides: 5,
+              },
+            },
+            size: {
+              value: 5,
+              random: true,
+              anim: {
+                enable: false,
+                speed: 40,
+                size_min: 0.1,
+                sync: false,
+              },
+            },
+            line_linked: {
+              enable: true,
+              distance: 150,
+              color: '#ffffff',
+              opacity: 0.4,
+              width: 1,
+            },
+            move: {
+              enable: true,
+              speed: 3,
+              direction: 'right',
+              random: true,
+              straight: false,
+              out_mode: 'bounce',
+              bounce: false,
+              attract: {
+                enable: false,
+                rotateX: 600,
+                rotateY: 1200,
+              },
+            },
+          },
+          interactivity: {
+            detect_on: 'canvas',
+            events: {
+              onhover: {
+                enable: true,
+                mode: 'repulse',
+              },
+              resize: true,
+            },
+            modes: {
+              repulse: {
+                distance: 150,
+                duration: 0.4,
+              },
+            },
+          },
+          retina_detect: true,
+        }
+      } />
+    );
+  }
+}
